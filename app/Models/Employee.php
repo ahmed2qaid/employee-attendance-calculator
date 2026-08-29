c42bb3270427325f04473da32ffc10d96ac76a4d
ahmed2qaid <ahmed0qaid@gmail.com>
2026-08-29 17:05:54 +0300

    Expose rotating shift fields

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 class Employee extends Model {
-    protected $fillable=['name','shift_type'];
+    protected $fillable=['name','shift_type','shift_mode','shift_one','shift_two','rotation_start'];
+    protected $casts=['rotation_start'=>'date'];
     public function attendances(): HasMany { return $this->hasMany(Attendance::class); }
 }
