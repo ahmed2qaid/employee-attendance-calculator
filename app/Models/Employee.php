df65ac3b85669106e70ad5becdc034704bb11ed0
ahmed2qaid <ahmed0qaid@gmail.com>
2026-08-29 16:16:07 +0300

    Add Employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+class Employee extends Model {
+    protected $fillable=['name'];
+    public function attendances(): HasMany { return $this->hasMany(Attendance::class); }
+}
