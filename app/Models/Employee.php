c61761f715c740d9254c837f8960d858ad902c81
ahmed2qaid <ahmed0qaid@gmail.com>
2026-08-29 16:52:04 +0300

    Allow configurable employee attendance policy

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 class Employee extends Model {
-    protected $fillable=['name'];
+    protected $fillable=['name','shift_type','late_grace','early_grace'];
+    protected $casts=['late_grace'=>'integer','early_grace'=>'integer'];
     public function attendances(): HasMany { return $this->hasMany(Attendance::class); }
 }
